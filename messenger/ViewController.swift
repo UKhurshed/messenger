6892823cbcaba02b1f4299241fab074dfc8d0bf5
UKhurshed <khurshedumarov777@gmail.com>
2022-12-13 22:02:56 +0500

    First commit

---
+++ messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  messenger
+//
+//  Created by Khurshed Umarov on 13.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
